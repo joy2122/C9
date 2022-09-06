3aaa225f51f625d50bddb3e0a29dc52e7d04c73f
joy2122 <112950407+joy2122@users.noreply.github.com>
2022-09-06 17:54:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+  
+function setup() {
+  createCanvas(600, 600);
+  var cloud=createSprite(300,300,30,30)
+   cloud.shapeColor="white"
+}
+
+
+function draw() {
+  background("skyblue");
+  drawSprites();
+}
